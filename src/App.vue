068448e0fba33da39359b416755d86475cc5cb08
Júlia Lima <juliafclima@hotmail.com>
2024-03-22 21:10:38 -0300

    feat: consuming data from the api

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,61 @@
 <template>
-  <HelloWorld msg="Gif Tok"/>
+  <div class="flex justify-center">
+    <div v-if="loading">Carregando...</div>
+    <div v-else-if="gif.images && gif.images.original">
+      <img :src="gif.images.original.url"
+        alt="Gif"
+        class="mx-5 w-80" />
+    </div>
+    <div v-else>Erro: Gif não encontrado</div>
+    <button @click="changeGif">Mudar</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/styles/index.css';
+import { GiphyFetch } from '@giphy/js-fetch-api';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data() {
+    return {
+      gif: {},
+      loading: true,
+    };
+  },
+
+  created() {
+    this.loadRandomGif();
+  },
+
+  methods: {
+    async loadRandomGif() {
+      const gf = new GiphyFetch(process.env.VUE_APP_GIPHY_API_KEY);
+
+      try {
+        const cacheBust = Date.now();
+
+        const { data: gif } = await gf.random({ tag: 'cat', cacheBust });
+
+        this.gif = gif;
+        this.loading = false;
+      } catch (error) {
+        console.error('Erro ao buscar o GIF:', error);
+
+        this.loading = false;
+
+        setTimeout(() => {
+          this.loading = true;
+          this.loadRandomGif();
+        }, 5000);
+      }
+    },
+
+    changeGif() {
+      this.loading = true;
+      this.loadRandomGif();
+    },
+  },
+};
+</script>
